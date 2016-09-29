4559893b0c1b9600dd56368054134e730c8c25b3
Dmitry Kolinchuk <dmko@sitecore.net>
2016-09-29 15:36:08 +0300

    Allow to prefill the Checkbox List field from the URL query string for MVC forms

---
+++ src/Sitecore.Support.67848/CheckboxListField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Support.Forms.Mvc.ViewModels.Fields
+{
+    public class CheckboxListField : Sitecore.Forms.Mvc.ViewModels.Fields.CheckboxListField
+    {
+        public override void SetValueFromQuery(string valueFromQuery)
+        {
+            if (string.IsNullOrEmpty(valueFromQuery))
+            {
+                return;
+            }
+            string[] array = valueFromQuery.Split(new char[]
+			{
+				','
+			});
+            string[] array2 = array;
+            for (int i = 0; i < array2.Length; i++)
+            {
+                string value = array2[i];
+                if (this.Items != null)
+                {
+                    foreach (SelectListItem current in this.Items)
+                    {
+                        if (current.Value.Equals(value, StringComparison.OrdinalIgnoreCase))
+                        {
+                            current.Selected = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.67848/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.67848")]
+[assembly: AssemblyProduct("Sitecore.Support.67848")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
